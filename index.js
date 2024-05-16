0520549e6959e6f4e3c17182b976b4b16b956b0a
Shashikant Singh <102726420+Shashikant-Singh54@users.noreply.github.com>
2024-05-17 01:49:05 +0530

    Resolved conflicts

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const jsonfile = require('jsonfile');
-const moment = require('moment')
-const simpleGit = require('simple-Git')
+const moment = require('moment');
+const simpleGit = require('simple-git');
 
+// Use dynamic import for ES module 'random'
+import('random').then(({ default: Random }) => {
+  const FILE_PATH = './data.json';
+  const random = new Random(); // Create an instance of Random
 
-const FILE_PATH = './data.json';
+  const makeCommit = n => {
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const DATE = moment().subtract(1, 'y').add(1, 'd')
+                  .add(x, 'w').add(y, 'd').format();
 
-const DATE = moment().format();
+    const data = {
+      date: DATE
+    };
 
-const data = {
-    date: DATE
-}
+    jsonfile.writeFile(FILE_PATH, data, () => {
+      simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
+        makeCommit.bind(this, --n)
+      );
+    });
+  }; 
 
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+  makeCommit(100);
+}).catch(error => {
+  // Handle any errors that may occur during the import
+});
